b1dc2d31c943a6a9455ba3961fab998b0b97a058
jzee <jzee21b@gmail.com>
2020-05-14 17:07:14 +0900

    Add values for logic

--- src/network/server/service/ServerService.java
+++ src/network/server/service/ServerService.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,14 +22,20 @@ import network.server.test.AbstractClient;
 import network.server.test.SharedMessage;
 import network.server.test.User;
 import network.server.vo.Message;
+import network.server.vo.Sensor;
 import network.server.vo.SensorData;
 
 
 public class ServerService {
 	
-	private static final Object MONITOR = new Object();
+	private Map<String, Device> deviceList = new ConcurrentHashMap<String, Device>();
+	private Map<String, Sensor> sensorList = new ConcurrentHashMap<String, Sensor>();
+	private List<String> typeHEAT = new ArrayList<String>();		// List<DeviceID>
+	private List<String> typeCOOL = new ArrayList<String>();
+	private List<String> typeTEMP = new ArrayList<String>();
+	private List<String> typeBED = new ArrayList<String>();
+	private List<String> typeLIGHT = new ArrayList<String>();
 	
-	private Map<String, Device> list = new ConcurrentHashMap<String, Device>();
 	private Gson gson=  new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").create();
 	
 	private ServerService() {}
@@ -40,28 +48,80 @@ public class ServerService {
 		return InstanceHandler.INSTANCE;
 	}
 	
-	//
-	public void add(Device c) {
-		this.list.put(c.getDeviceID(), c);
+	
+	// method
+	public Device add(String deviceID, Socket socket) {
+//		this.list.put(c.getDeviceID(), c);
+		Device device = null;
+		if((device = deviceList.get(deviceID)) == null) {
+			/* First connection check : true
+			 * Create new Device.class			*/
+			device = new Device(deviceID, socket);
+			deviceList.put(deviceID, device);
+		} else {
+			/* First connection check : false
+			 * Update the socket				*/
+			device.setSocket(socket);
+		}
+		return device;
 	}
 	
 	public void remove(Device c) {
-		this.list.remove(c.getDeviceID());
+		this.deviceList.remove(c.getDeviceID());
 	}
 	
 	public Device get(String id) {
-		return this.list.get(id);
+		return this.deviceList.get(id);
 	}
 	
 	public Map<String, Device> getList() {
-		return this.list;
+		return this.deviceList;
 	}
 	
-	public void dataHandler(String jsonData) {
+	public void setDeviceSensorList(Device device, Message data) {
+		Map<String, Sensor> map = gson.fromJson(data.getJsonData(), HashMap.class);
+		
+		for(String key : map.keySet()) {
+			
+			Sensor sensor = sensorList.putIfAbsent(key, map.get(key));
+			String id = sensor.getDeviceID();		// Sensor's DeviceID
+			
+			switch (sensor.getSensorType()) {
+			case "HEAT":
+				if(!typeHEAT.contains(id))
+					typeHEAT.add(id);
+				break;
+			case "COOL":	
+				if(!typeCOOL.contains(id))
+					typeCOOL.add(id);
+				break;
+			case "TEMP":
+				if(!typeTEMP.contains(id))
+					typeTEMP.add(id);
+				break;
+			case "BED":
+				if(!typeBED.contains(id))
+					typeBED.add(id);
+				break;
+			case "LIGHT":
+				if(!typeLIGHT.contains(id))
+					typeLIGHT.add(id);
+				break;
+
+			default:
+				break;
+			} // switch
+		} // for
+	}
+	
+	public void dataHandler(Device receiver, String jsonData) {
 		Message data = gson.fromJson(jsonData, Message.class);
 		System.out.println(data.getDeviceID() + " : " + data.getJsonData());
 		
-		if (data.getDataType().equals("SensorData")) {
+		if (data.getDataType().equals("SENSOR_LIST")) {
+			setDeviceSensorList(receiver, data);
+			
+		} else if (data.getDataType().equals("SensorData")) {
 //			SensorData sensorData = gson.fromJson(data.getJsonData(), SensorData.class);
 			SensorData sensorData = data.getSensorData();
 		}
