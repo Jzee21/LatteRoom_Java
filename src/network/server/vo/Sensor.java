7deb72aa20f48d38299dca68db53aaaa13e7856b
jzee <jzee21b@gmail.com>
2020-05-14 17:05:02 +0900

    Add initial value

--- src/network/server/vo/Sensor.java
+++ src/network/server/vo/Sensor.java
@@ -6,7 +6,7 @@ public class Sensor {
 	private String deviceID;
 	private String sensorID = "" + this.hashCode();
 	private String sensorType;
-	private SensorData recentData;
+	private SensorData recentData = new SensorData(this.sensorID);
 	
 	
 	// constructor
