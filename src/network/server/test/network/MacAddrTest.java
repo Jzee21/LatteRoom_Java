b8f3a94dea4ca26c9d006f542d8a710636e07c39
jzee <jzee21b@gmail.com>
2020-06-08 16:03:03 +0900

    Max size - Mac to Int

--- src/network/server/test/network/MacAddrTest.java
+++ src/network/server/test/network/MacAddrTest.java
@@ -83,6 +83,19 @@ public class MacAddrTest {
 		System.out.println("Mac Addr : " + test.getLocalMacAddress());
 		System.out.println("Mac Addr : " + Integer.toHexString(test.getIntLocalMacAdderss()));
 		System.out.println("Mac Addr : " + test.getIntLocalMacAdderss());
+		
+		System.out.println("" + Integer.parseInt("24F5AA", 16));	// 2422186
+		System.out.println("" + Integer.parseInt("FFFFFF", 16));	// 16777215
+		System.out.println("" + Integer.parseInt("FFFFFFF", 16));	// 268435455
+		System.out.println("" + Integer.parseInt("1FFFFFFF", 16));	// 536870911
+		System.out.println("" + Integer.parseInt("2FFFFFFF", 16));	// 805306367
+		System.out.println("" + Integer.parseInt("3FFFFFFF", 16));	// 1073741823
+		System.out.println("" + Integer.parseInt("4FFFFFFF", 16));	// 1342177279
+		System.out.println("" + Integer.parseInt("5FFFFFFF", 16));	// 1610612735
+		System.out.println("" + Integer.parseInt("6FFFFFFF", 16));	// 1879048191
+		System.out.println("" + Integer.parseInt("7FFFFFFF", 16));	// 2147483647
+		System.out.println("" + Integer.parseInt("8FFFFFFF", 16));	// Error
+		System.out.println("" + Integer.parseInt("FFFFFFFF", 16));
 	}
 
 }
