58c761152326a72291da54e15fc6af9f5e3506db
jzee <jzee21b@gmail.com>
2020-06-13 18:10:01 +0900

    Test Target transfer

--- src/network/server3/controller/ServerController.java
+++ src/network/server3/controller/ServerController.java
@@ -1,22 +1,49 @@
 package network.server3.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import network.server3.dao.*;
+import network.server3.vo.*;
 
 public class ServerController {
 
 	// =================================================
 	// field
+	private static final int BUFFSIZE = 20480;
+	
 	private Selector selector = null;
 	private Vector<SocketChannel> channels = new Vector<SocketChannel>();
+	private ConcurrentHashMap<SocketChannel, String> userChannel = 
+			new ConcurrentHashMap<SocketChannel, String>();
+	private ConcurrentHashMap<SocketChannel, String> deviceChannel = 
+			new ConcurrentHashMap<SocketChannel, String>();
+	
+	private Controller deviceController = DeviceController.getInstance();
+	private Controller guestController = GuestController.getInstance();
+	
+	private GuestDAO guestDao = new GuestDAO();
+	
+	private Charset charset = Charset.forName("UTF-8");
+	private CharsetDecoder decoder = charset.newDecoder();
+	private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").create();
 	
 	
 	// =================================================
@@ -64,9 +91,10 @@ public class ServerController {
 
 	                if (selectionKey.isAcceptable()) {
 	                    accept(selectionKey);
-	                }
-	                if (selectionKey.isReadable()) {
+	                } else if (selectionKey.isReadable()) {
 	                    read(selectionKey);
+	                } else if (selectionKey.isWritable()) {
+//	                	send(selectionKey);
 	                }
 
 	                iterator.remove();
@@ -116,30 +144,70 @@ public class ServerController {
 	private void read(SelectionKey key) {
 		// Get a socket channel from SelectionKey
 		SocketChannel channel = (SocketChannel) key.channel();
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(BUFFSIZE);
         
         try {
 			int count = channel.read(byteBuffer);	// Reading data from the socket
 			if(count < 0) {							// Socket closed from client
-//				System.out.println(channel.getRemoteAddress().toString() + "] closed");
-//				channel.close();
 				closeChannel(channel);
 				key.cancel();
 				return;
 			}
 			
 			// test
-			broadcast(byteBuffer);
+//			broadcast(byteBuffer);
+			
+			byteBuffer.flip();
+            String jsonString = decoder.decode(byteBuffer).toString();
+//            System.out.print(jsonString);
+//            broadcast(jsonString);
+            Message data = gson.fromJson(jsonString, Message.class);
+            System.out.println(data.toString());
+            
+            switch (data.getCode1()) {
+			/** Device */
+			// Device Connect
+			case "CONNECT":
+				
+				break;
+			// Device Change States
+			case "UPDATE":
+				deviceController.dataHandler(data);
+				break;
+			
+			/** Guest */
+			// Guest Login
+			case "LOGIN":
+				System.out.println("Code : LOGIN");
+				connectUser(channel, data.getJsonData());
+				break;
+			// Change channel
+			case "RECONNECT":
+				
+				break;
+			// Guest requests device control
+			case "CONTROL":
+				
+				break;
+			//
+			case "RESERVLIST":
+				
+				break;
+			//
+			case "ROOMDETAIL":
+				
+				break;
+			//
+			case "ALARM":
+				
+				break;
+			default:
+				break;
+			}
+			
+			
 			
 		} catch (IOException e) {
-//			e.printStackTrace();
-//			try {
-//				System.out.println(channel.getRemoteAddress().toString() + "] closed");
-//				channel.close();
-//				key.cancel();
-//			} catch (IOException e1) {
-//				e1.printStackTrace();
-//			}
 			closeChannel(channel);
 			key.cancel();
 		}
@@ -154,6 +222,116 @@ public class ServerController {
 		}
 	}
 	
+	private void connectUser(SocketChannel channel, String jsonData) {
+		System.out.println("connectUser");
+		Guest input = gson.fromJson(jsonData, Guest.class);
+		Guest result = guestDao.checkLogin(input);
+		
+		System.out.println("connectUser : Message");
+		Message data = null;
+		if(result != null) {
+			userChannel.put(channel, result.getUserNo());
+			data = new Message(result.getUserNo(), "LOGIN", "SUCCESS", gson.toJson(result));
+		} else {
+			data = new Message(null, "LOGIN", "FAILE", null);
+		}
+		System.out.println("connectUser : Send " + data.toString());
+//		send(channel, gson.toJson(jsonData));
+		
+		try {
+			broadcast(gson.toJson(jsonData));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		try {
+//			channel.register(selector, SelectionKey.OP_WRITE, gson.toJson(data));
+//		} catch (ClosedChannelException e) {
+//			System.out.println("전송 등록 실패");
+//			e.printStackTrace();
+//		}
+		
+	}
+	
+	private void send(SocketChannel channel, String jsonData) {
+		
+		ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFSIZE);
+		buffer.clear();
+		buffer.put(jsonData.getBytes());
+		buffer.flip();
+		
+//		Iterator iterator = channels.iterator();
+//		while (iterator.hasNext()) {
+//			SocketChannel socketChannel = (SocketChannel) iterator.next();
+//			if (socketChannel != null) {
+//				socketChannel.write(buffer);
+//				buffer.rewind();
+//				}
+//			}
+		if(channel != null) {
+			try {
+				channel.write(buffer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			buffer.rewind();
+		}
+		buffer.clear();
+		
+	}
+	
+//	private void send(SelectionKey key) {
+//		
+//		try {
+//			SocketChannel channel = (SocketChannel) key.channel();
+//			channel.configureBlocking(true);
+//			
+//			String jsonData = (String) key.attachment();
+//			System.out.println("before send : " + jsonData);
+////			ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFSIZE);
+////			buffer.clear();
+////			buffer.put(jsonData.getBytes());
+////			buffer.flip();
+////			channel.write(buffer);
+////			buffer.clear();
+//			
+//			Socket socket = channel.socket();
+//			PrintWriter out = new PrintWriter(socket.getOutputStream());
+//			out.println(jsonData);
+//			out.flush();
+//			out.close();
+//			
+//			channel.configureBlocking(false);
+//			channel.register(selector, SelectionKey.OP_READ);
+//			System.out.println("buffer clear");
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+	
+//	private void send(SocketChannel channel, Message data) {
+//		if(channel != null && data != null) {
+//			System.out.println("보내자");
+//			try {
+//				ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFSIZE);
+//				buffer.clear();
+//				
+//				String jsonString = gson.toJson(data);
+//				buffer.put(jsonString.getBytes());
+//				buffer.flip();
+//				
+//				channel.write(buffer);
+//				buffer.clear();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+	
+	
 	private void broadcast(ByteBuffer byteBuffer) throws IOException {
         byteBuffer.flip();
         Iterator iterator = channels.iterator();
@@ -167,6 +345,28 @@ public class ServerController {
             }
         }
         byteBuffer.clear();
+    }
+	
+	private void broadcast(String data) throws IOException {
+		ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFSIZE);
+//        byteBuffer.flip();
+//        byteBuffer.clear();
+//        byteBuffer.put(message.getBytes());
+//        byteBuffer.flip();
+		buffer.clear();
+		buffer.put(data.getBytes());
+		buffer.flip();
+		
+        Iterator iterator = channels.iterator();
+        while (iterator.hasNext()) {
+            SocketChannel socketChannel = (SocketChannel) iterator.next();
+
+            if (socketChannel != null) {
+                socketChannel.write(buffer);
+                buffer.rewind();
+            }
+        }
+        buffer.clear();
     }
 	
 }
