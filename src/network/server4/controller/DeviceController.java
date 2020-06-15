3c898d376ee7486ded5f16c969153d10a1f6056b
Jzee21 <jzee21b@gmail.com>
2020-06-16 01:44:34 +0900

    Add methods in Controllers

--- src/network/server4/controller/DeviceController.java
+++ src/network/server4/controller/DeviceController.java
@@ -1,10 +1,17 @@
 package network.server4.controller;
 
+import network.server4.vo.Message;
+
 public class DeviceController {
 
 	// =================================================
+	// field
+	private Dispatcher dispatcher;
+	
+	// =================================================
 	// Singleton
 	private DeviceController() {
+		dispatcher = Dispatcher.getInstance();
 	}
 	
 	private static class InstanceHandler {
@@ -18,7 +25,19 @@ public class DeviceController {
 	
 	// =================================================
 	// methods
+	public void upToDate(Connection conn, Message data) {
+		// Message {deviceNo, UPDATE, TYPE, SensorData{sensorNo, time, states, stateDetail}}
+		
+		// Insert new SensorData
+		// Deliver information to guests
+	}
 	
+	public void requestControl(Connection conn, Message data) {
+		// Message {roomNo, CONTROL, TYPE, SensorData{time, states, stateDetail}}
+		
+		// Insert new ControlData
+		// Deliver information to device
+	}
 	
 	
 }
--- src/network/server4/controller/GuestController.java
+++ src/network/server4/controller/GuestController.java
@@ -1,10 +1,21 @@
 package network.server4.controller;
 
+import java.util.List;
+
+import network.server4.dao.ReservDAO;
+import network.server4.vo.Message;
+import network.server4.vo.Reservation;
+
 public class GuestController {
 
 	// =================================================
+	// field
+	private Dispatcher dispatcher;
+	
+	// =================================================
 	// Singleton
 	private GuestController() {
+		dispatcher = Dispatcher.getInstance();
 	}
 	
 	private static class InstanceHandler {
@@ -18,7 +29,36 @@ public class GuestController {
 	
 	// =================================================
 	// methods
+	public void requestReserv(Connection conn, Message data) {
+		// Message {userNo, RESERVLIST, null, null}
+		
+//		List<Reservation> reservInfo;
+	}
 	
+	public void requestRoomDetial(Connection conn, Message data) {
+		// Message {userNo, ROOMDETAIL, null, Room{roomNo}}
+		
+	}
+	
+	public void requestAlarmInfo(Connection conn, Message data) {
+		// Message {userNo, ALARM, GET, null}
+		
+	}
+	
+	public void requestAlarmSet(Connection conn, Message data) {
+		// Message {userNo, ALARM, UPDATE, Alarm{alarmNo, hour, min, weeks, flag}}
+		
+	}
+	
+	public void requestAlarmJobInfo(Connection conn, Message data) {
+		// Message {userNo, ALARMJOB, GET, null}
+		
+	}
+	
+	public void requestAlarmJobSet(Connection conn, Message data) {
+		// Message {userNo, ALARMJOB, UPDATE, List[AlarmData{dataNo, alarmNo, type, states, stateDetial}]}
+		
+	}
 	
 	
 }
