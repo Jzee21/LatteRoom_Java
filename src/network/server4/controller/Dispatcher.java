9aa1bfc90e20e574a1633dcf8efefd107e6dba07
Jzee21 <jzee21b@gmail.com>
2020-06-16 01:44:01 +0900

    fix Switch(code1)

--- src/network/server4/controller/Dispatcher.java
+++ src/network/server4/controller/Dispatcher.java
@@ -48,50 +48,74 @@ public class Dispatcher {
 		Connection conn = null;
 		conn = new Connection(socket);
 		return conn;
-	}
+		
+	} // accept()
 	
 	public void read(Connection conn, String jsonData) {
 //		broadcast(jsonData);
 		
 		try {
 			Message data = gson.fromJson(jsonData, Message.class);
+			if(data.getCode1() != null) data.setCode1(data.getCode1().toUpperCase());
+			if(data.getCode2() != null) data.setCode2(data.getCode2().toUpperCase());
+			
 			switch (data.getCode1()) {
 			/** Device */
 			// Device Connect
 			case "CONNECT":
-				addDevice(conn, data);
+				deviceAuth(conn, data);
 				break;
+				
 			// Device status changes
 			case "UPDATE":
 //				deviceController.dataHandler(data);
+				dController.upToDate(conn, data);
 				break;
 			
+				
 			/** Guest */
 			// Guest Login
 			case "LOGIN":
 				System.out.println("LOGIN : " + gson.fromJson(data.getJsonData(), Guest.class));
-				addGuest(conn, data);
+				guestAuth(conn, data);
 				break;
-			// Change channel
+				
+			// Connection renewal
 			case "RECONNECT":
-				reconnGuest(conn, data);
+				guestReconn(conn, data);
 				break;
+				
 			// Guest requests device control
 			case "CONTROL":
-				
+				dController.requestControl(conn, data);
 				break;
-			//
+				
+			// Guest requests reservation list
 			case "RESERVLIST":
-				
+				gController.requestReserv(conn, data);
 				break;
-			//
+				
+			// Guest requests reserved room info
 			case "ROOMDETAIL":
-				
+				gController.requestRoomDetial(conn, data);
 				break;
-			//
+				
+			// Guest requests personal alarm info
 			case "ALARM":
-				
+				if(data.getCode2().equals("GET"))
+					gController.requestAlarmInfo(conn, data);
+				else
+					gController.requestAlarmSet(conn, data);
 				break;
+				
+			// Guest requests detail alarm infos
+			case "ALARMJOB":
+				if(data.getCode2().equals("GET"))
+					gController.requestAlarmJobInfo(conn, data);
+				else
+					gController.requestAlarmJobSet(conn, data);
+				break;
+				
 			default:
 				break;
 			}
@@ -100,7 +124,8 @@ public class Dispatcher {
 //			e.printStackTrace();
 		}
 		
-	}
+	} // read()
+	
 	
 	public void removeOne(Connection conn) {
 		LatteServer.getConnections().remove(conn);
@@ -116,6 +141,18 @@ public class Dispatcher {
 				break;
 			}
 		}
+	} // removeOne()
+	
+	public Connection getDeviceConn() {
+		Connection conn = null;
+		
+		return conn;
+	}
+	
+	public Connection getGuestConn() {
+		Connection conn = null;
+		
+		return conn;
 	}
 	
 	public void broadcast(String data) {
@@ -129,13 +166,16 @@ public class Dispatcher {
 		deviceList.clear();
 	}
 	
+	
 	// =================================================
 	// private methods
-	private void addGuest(Connection conn, Message data) {
+	private void guestAuth(Connection conn, Message data) {
 		/** Member registration confirmation */
+		// Message {null, LOGIN, null, Guest{null, ID, PW, null, null}}
+		
 		// Registration Information Search
 		Guest input = gson.fromJson(data.getJsonData(), Guest.class);
-		Guest result = gdao.checkLogin(input);
+		Guest result = gdao.getGuest(input);
 		
 		if(result != null) {
 			// Registered guest
@@ -151,9 +191,13 @@ public class Dispatcher {
 
 		// Send request results
 		conn.send(data);
+		
 	} // addGuest()
 	
-	private void reconnGuest(Connection conn, Message data) {
+	
+	private void guestReconn(Connection conn, Message data) {
+		// Message {guestNo, RECONNECT, null, null}
+		
 		Connection prev = guestList.get(data.getClientNo());
 		if(prev != null) {
 			prev.close();
@@ -163,9 +207,12 @@ public class Dispatcher {
 		conn.setType("GUEST");
 		guestList.put(conn.getClientNo(), conn);
 		
-	}
+	} // guestReconn()
 	
-	private void addDevice(Connection conn, Message data) {
+	
+	private void deviceAuth(Connection conn, Message data) {
+		// Message {deviceNo, CONNECT, null, null}
+		
 		String input = data.getClientNo();
 		int count = 1;	// dao
 		
@@ -181,6 +228,6 @@ public class Dispatcher {
 			conn.setType("DEVICE");
 			deviceList.put(conn.getClientNo(), conn);
 		}
-	}
+	} // deviceAuth()
 	
 }
