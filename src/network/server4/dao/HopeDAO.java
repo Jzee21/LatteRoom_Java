2c46d9534c11521b3a21b32c77d9c48c9467fe77
jzee <jzee21b@gmail.com>
2020-06-15 19:57:55 +0900

    Update HopeDAO

---
+++ src/network/server4/dao/HopeDAO.java
@@ -0,0 +1,46 @@
+package network.server4.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import network.server4.mybatis.MyBatisFactory;
+import network.server4.vo.Hope;
+
+public class HopeDAO {
+	
+	public Hope getHope(String userNo) {
+		try (SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
+			return sqlSession.selectOne("HopeMapper.selectItem", userNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public int updateTemp(String userNo, String states) {
+		try (SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
+			return sqlSession.update("HopeMapper.updateTemp", new Hope(userNo, states, null, null));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public int updateLight(String userNo, String states) {
+		try (SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
+			return sqlSession.update("HopeMapper.updateLight", new Hope(userNo, null, states, null));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public int updateBlind(String userNo, String states) {
+		try (SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
+			return sqlSession.update("HopeMapper.updateBlind", new Hope(userNo, null, null, states));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+}
--- src/network/server4/test/HopeUpdate.java
+++ src/network/server4/test/HopeUpdate.java
@@ -2,6 +2,7 @@ package network.server4.test;
 
 import org.apache.ibatis.session.SqlSession;
 
+import network.server4.dao.HopeDAO;
 import network.server4.mybatis.MyBatisFactory;
 import network.server4.vo.Hope;
 
@@ -9,18 +10,28 @@ public class HopeUpdate {
 
 	public static void main(String[] args) {
 		
-		int result;
-		try(SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
-//			String[] input = {"GUEST0002", "27"};
-//			String[] input = {"27", "GUEST0002"};
-//			String input1 = "GUEST0002";
-//			String input2 = "27";
-			Hope input = new Hope("GUEST0002", "27", null, null);
-			result = sqlSession.update("HopeMapper.updateTemp", input);
-		} catch (Exception e) {
-			e.printStackTrace();
-			result = 0;
-		}
+//		int result;
+//		try(SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
+//			Hope input = new Hope("GUEST0002", "27", null, null);
+//			result = sqlSession.update("HopeMapper.updateTemp", input);
+//
+//			Hope input2 = new Hope("GUEST0002", null, "50", null);
+//			result = sqlSession.update("HopeMapper.updateLight", input2);
+//			
+//			Hope input3 = new Hope("GUEST0002", null, null, "CLOSE");
+//			result = sqlSession.update("HopeMapper.updateBlind", input3);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			result = 0;
+//		}
+		HopeDAO hdao = new HopeDAO();
+//		int result = hdao.updateTemp(new Hope("GUEST0002", "26", null, null));
+//		int result = hdao.updateLight(new Hope("GUEST0002", null, "50", null));
+//		int result = hdao.updateBlind(new Hope("GUEST0002", null, null, "OPEN"));
+		
+//		int result = hdao.updateTemp("GUEST0002", "27");
+//		int result = hdao.updateLight("GUEST0002", "60");
+		int result = hdao.updateBlind("GUEST0002", "CLOSE");
 		
 		System.out.println("실행결과 : " + result);
 
