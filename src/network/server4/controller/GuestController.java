aaa6c7992e2bb5c872e3c91d03697dd935f2f2c6
jzee <jzee21b@gmail.com>
2020-06-16 14:26:42 +0900

    Update Logic - List of Reservation, Room Detail Info

--- src/network/server4/controller/GuestController.java
+++ src/network/server4/controller/GuestController.java
@@ -1,16 +1,25 @@
 package network.server4.controller;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
-import network.server4.dao.ReservDAO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import network.server4.service.ReservService;
+import network.server4.service.RoomService;
 import network.server4.vo.Message;
 import network.server4.vo.Reservation;
+import network.server4.vo.Room;
+import network.server4.vo.RoomDetail;
 
 public class GuestController {
 
 	// =================================================
 	// field
 	private Dispatcher dispatcher;
+	private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 	
 	// =================================================
 	// Singleton
@@ -32,11 +41,34 @@ public class GuestController {
 	public void requestReserv(Connection conn, Message data) {
 		// Message {userNo, RESERVLIST, null, null}
 		
-//		List<Reservation> reservInfo;
+		ReservService rService = new ReservService();
+		
+		List<Reservation> reservInfo = rService.getReservInfo(data.getClientNo());
+		
+		Message response = 
+				new Message(data.getClientNo(), 
+						data.getCode1(), 
+						Integer.toString(reservInfo.size()), 
+						gson.toJson(reservInfo));
+		
+		conn.send(response);
 	}
 	
 	public void requestRoomDetial(Connection conn, Message data) {
 		// Message {userNo, ROOMDETAIL, null, Room{roomNo}}
+		RoomService rService = new RoomService();
+		
+		Room input = gson.fromJson(data.getJsonData(), Room.class);
+		
+		RoomDetail roomInfo = rService.getRoomDetail(data.getClientNo(), input);
+		
+		Message response = 
+				new Message(data.getClientNo(), 
+						data.getCode1(), 
+						"SUCCESS", 
+						gson.toJson(roomInfo));
+		
+		conn.send(response);
 		
 	}
 	
@@ -45,7 +77,7 @@ public class GuestController {
 		
 	}
 	
-	public void requestAlarmSet(Connection conn, Message data) {
+	public void requestAlarmUpdate(Connection conn, Message data) {
 		// Message {userNo, ALARM, UPDATE, Alarm{alarmNo, hour, min, weeks, flag}}
 		
 	}
@@ -55,7 +87,7 @@ public class GuestController {
 		
 	}
 	
-	public void requestAlarmJobSet(Connection conn, Message data) {
+	public void requestAlarmJobUpdate(Connection conn, Message data) {
 		// Message {userNo, ALARMJOB, UPDATE, List[AlarmData{dataNo, alarmNo, type, states, stateDetial}]}
 		
 	}
