d016ccccbc17dd41f1aee33b14f05f38d74c9c4a
Jzee21 <jzee21b@gmail.com>
2020-06-17 01:40:25 +0900

    Update - fine no value

--- src/network/server4/dao/SensorDAO.java
+++ src/network/server4/dao/SensorDAO.java
@@ -5,10 +5,21 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 
 import network.server4.mybatis.MyBatisFactory;
+import network.server4.vo.NoKey;
 import network.server4.vo.Sensor;
 import network.server4.vo.SensorData;
 
 public class SensorDAO {
+	
+	public String selectSensorNo(String roomNo, String type) {
+		try (SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
+			NoKey input = new NoKey(roomNo, type);
+			return sqlSession.selectOne("SensorMapper.selectSensorNo", input);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
 	public Sensor selectSensorOne(String sensorNo) {
 		try (SqlSession sqlSession = MyBatisFactory.getSqlSession()) {
